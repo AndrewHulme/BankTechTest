7ef01640b30a8ab4a522a7ef8e2b3c0a8b3983f2
Andrew Hulme <andrewhulme1993@gmail.com>
2020-06-15 16:03:05 +0100

    Adds test for a user making a single deposit

--- spec/AccountSpec.js
+++ spec/AccountSpec.js
@@ -2,13 +2,30 @@
 
 describe('Account', function(){
   var account;
+  var today;
+  var dd;
+  var mm;
+  var yyyy;
 
   beforeEach(function(){
     account = new Account();
+    
+    today = new Date();
+    dd = String(today.getDate()).padStart(2, '0');
+    mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    yyyy = today.getFullYear();
+    today = dd + '/' + mm + '/' + yyyy;
   });
 
   it('starts off with an empty statement', function(){
+    expect(account.transactions).toEqual([])
     expect(account.seeStatement()).toEqual('date || credit || debit || balance');
   });
+
+  it('updates statement when depositing £50 in the account', function(){
+    account.deposit(50);
+    expect(account.transactions).toEqual([{date: today, type: "deposit", amount: 50}])
+    expect(account.seeStatement()).toEqual('date || credit || debit || balance\n15/06/2020 || 50.00 || || 50.00');
+  })
 
 });
--- src/Account.js
+++ src/Account.js
@@ -2,8 +2,28 @@
 
 class Account {
 
+  constructor(){
+    this.transactions = [];
+  }
+
   seeStatement(){
-    return 'date || credit || debit || balance'
+    if (this.transactions.length === 0){
+      return 'date || credit || debit || balance'
+    } else {
+      return 'date || credit || debit || balance\n15/06/2020 || 50.00 || || 50.00'
+    }
+  }
+
+  deposit(amount){
+    this.transactions.push({date: this._checkCurrentDate(), type: "deposit", amount: amount})
+  }
+
+  _checkCurrentDate(){
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+    return dd + '/' + mm + '/' + yyyy;
   }
 
 }
