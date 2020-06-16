5b16aa0a99dd97d3c1819e399b2dce4b25099c2f
Andrew Hulme <andrewhulme1993@gmail.com>
2020-06-16 10:16:36 +0100

    Adds another test for withdrawal

--- spec/AccountSpec.js
+++ spec/AccountSpec.js
@@ -48,4 +48,12 @@ describe('Account', function(){
     expect(account.seeStatement()).toEqual(`date || credit || debit || balance\n${today} || || 50.00 || 50.00\n${today} || 100.00 || || 100.00`);
   })
 
+  it('updates statement when depositing £5000 then withdrawing £100 from the account', function(){
+    account.deposit(5000.00);
+    account.withdraw(100.00);
+    account.withdraw(200.00);
+    expect(account.transactions).toEqual([{date: today, type: "deposit", amount: 5000.00}, {date: today, type: "withdrawal", amount: 100.00}, {date: today, type: "withdrawal", amount: 200.00}])
+    expect(account.seeStatement()).toEqual(`date || credit || debit || balance\n${today} || || 200.00 || 4700.00\n${today} || || 100.00 || 4900.00\n${today} || 5000.00 || || 5000.00`);
+  })
+
 });
